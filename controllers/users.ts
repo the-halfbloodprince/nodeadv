af1dc789794850ac715b49193bf8142da5a44e6b
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-05-26 04:29:01 +0530

    add fixes in controllers

--- controllers/users.ts
+++ controllers/users.ts
@@ -10,11 +10,13 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const addNewUser = async (req: Request, res: Response) => {
     const newUserFromReq = req.body
     insertUserSchema.parse(newUserFromReq)
-    await app.db.insert(users).values(newUserFromReq)
+    const newUser = await app.db.insert(users).values(newUserFromReq).returning()
+    res.send(newUser)
 }
 
 export const updateUser = async (req: Request, res: Response) => {
     const userFromReq = req.body
     updateUserSchema.parse(userFromReq)
-    await app.db.update(users).set(userFromReq)
+    const updatedUser = await app.db.update(users).set(userFromReq).returning()
+    res.send(updatedUser)
 }
