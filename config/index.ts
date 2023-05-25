56158f28e6104bb17af95755439e988c1bd72356
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-05-26 04:27:03 +0530

    removed migrationClient from app config

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,6 @@ import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 type AppConfig = {
     dbUrl: string
     db: PostgresJsDatabase
-    migrationDBClient?: PostgresJsDatabase
 }
 
 export const appConfig: AppConfig = {
