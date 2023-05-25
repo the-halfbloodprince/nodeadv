a1546539a7fc745671515ce5bf1d221636a01929
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-05-26 04:02:16 +0530

    controllers and config

--- config/index.ts
+++ config/index.ts
@@ -1,31 +1,10 @@
-// import { Options } from 'sequelize'
-// import type { PrismaClient } from '@prisma/client'
+import type { PrismaClient } from '@prisma/client'
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 
-// const dbSettings: Options = {
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'dev1',
-//     username: 'mkdjff',
-//     password: 'xD'
-// }
-
-// export const appConfig = {
-//     dbSettings,
-//     pool: null
-// }
-
 type AppConfig = {
-    // Prisma
-    // db: PrismaClient
-
-    // Drizzle
-    dbUrl: string
-    db: PostgresJsDatabase
-    migrationDBClient?: PostgresJsDatabase
+    db: PrismaClient
 }
 
 export const appConfig: AppConfig = {
-    dbUrl: null,
     db: null
 }
--- controllers/users.ts
+++ controllers/users.ts
@@ -1,20 +1,45 @@
-import { Request, Response } from "express";
-import { appConfig as app } from "../config";
-import { insertUserSchema, updateUserSchema, users } from "../models/user";
+import { Request, Response } from "express"
+import type { PrismaClient, User } from '@prisma/client'
+import { appConfig as app } from "../config"
 
 export const getAllUsers = async (req: Request, res: Response) => {
-    const allUsers = await app.db.select().from(users)
-    res.send(allUsers)
+    try {
+        const allUsers = await app.db.user.findMany()
+        res.send(allUsers)
+    } catch (ex) {
+        res.status(500).send('Couldnt retrieve data from db')
+    }
 }
 
 export const addNewUser = async (req: Request, res: Response) => {
-    const newUserFromReq = req.body
-    insertUserSchema.parse(newUserFromReq)
-    await app.db.insert(users).values(newUserFromReq)
+    try {
+        await app.db.user.create({
+            data: {
+                name: 'mkd',
+                email: 'manissdsh@ghui.com',
+                roll_no: '19gg20017',
+                posts: {
+                    create: { title: 'first post' },
+                },
+                profile: {
+                    create: { bio: 'I love penguing' }
+                }
+            }
+        })
+        res.send('Added')
+    } catch (ex) {
+        res.status(500).send(ex)
+    }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
-    const userFromReq = req.body
-    updateUserSchema.parse(userFromReq)
-    await app.db.update(users).set(userFromReq)
+    try {
+        const user = await app.db.user.update({
+            where: { id: 1 },
+            data: { roll_no: '19xx20909' }
+        })
+        res.send(user)
+    } catch(ex) {
+        res.send(500).send(ex)
+    }
 }
